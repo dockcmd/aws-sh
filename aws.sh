e409f2b1debab08d77080d90ad200947f22b49e2
Mona Lisa <srdrucker@gmail.com>
2021-02-13 00:39:34 -0500

    v3

--- aws.sh
+++ aws.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
-# must be run under /bin/bash or <(env) below will cause an unexpected token `('
-
 . shmod
-import dockcmd/sh@v0.0.2 docker.sh
+import dockcmd/sh@v0.0.3 docker.sh
 
-exec_or_dryrun \
+shmod_exec \
   $(docker_run) \
   --env-file <(env|grep ^AWS_) \
-  --mount type=bind,source=$HOME/.aws,target=/root/.aws,consistency=delegated \
-  --mount type=bind,source=$HOME/.kube,target=/root/.kube,consistency=delegated \
-  $(docker_workdir) \
+  $(docker_home_workdir) \
   $(docker_image dcmd/aws:v1.16.306) \
   "$@"
