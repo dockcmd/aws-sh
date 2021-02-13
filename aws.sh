f92978ce90c6a1a2baa26cfd4027a6d476f3119a
Mona Lisa <srdrucker@gmail.com>
2021-02-13 11:36:49 -0500

    single docker function

--- aws.sh
+++ aws.sh
@@ -1,10 +1,5 @@
-#!/bin/bash
+#!/bin/sh
 . shmod
 import dockcmd/sh@v0.0.3 docker.sh
 
-run \
-  $(docker_run) \
-  --env-file <(env|grep ^AWS_) \
-  $(docker_home_workdir) \
-  $(docker_image dcmd/aws:v1.16.306) \
-  "$@"
+run `e=^AWS_ hwd= docker dcmd/aws:v1.16.306 "$@"`
